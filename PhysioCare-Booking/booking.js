b807620d41a39a939ecad8d72b29223e343bb792
eyad7330 <83145095+eyad7330@users.noreply.github.com>
2025-08-09 20:16:22 +0300

    Delete PhysioCare-Booking directory

--- PhysioCare-Booking/booking.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  // Hide form and show confirmation
-  document.getElementById("bookingForm").style.display = "none";
-  document.getElementById("confirmationMessage").classList.remove("hidden");
-
-  // (اختياري) تقدر تبعت داتا على واتساب هنا
-});
